432fa1ab8aabb1f826717c4b7de59fab2546e630
Abid <ahmedabid3409@gmail.com>
2022-05-03 23:48:52 +0600

    JSX, React Element and Render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+//React JSX element and REact rendering
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//const hello=React.createElement("h1",{className:"Hey"},"I am Abid");//create element of React!!
+//using JSX React will automatically make the JSX element turn into the create element of react
+const element=(
+  <h1 className='first react'>Hey I am Abid</h1>
 );
+console.log(element);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element,document.querySelector(".root"));
